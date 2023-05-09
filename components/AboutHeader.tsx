fc4f922f5265ddc9dfead6075a601e8ce6fc58e1
hypecarson <carson.lecky@gmail.com>
2023-05-09 14:34:54 -0700

    ye

--- components/AboutHeader.tsx
+++ components/AboutHeader.tsx
@@ -10,7 +10,7 @@ export default function AboutHeader() {
             <div className="mx-auto max-w-7xl px-6 py-12 lg:px-8">
                 <div className="mx-auto max-w-2xl lg:mx-0 lg:grid lg:max-w-none lg:grid-cols-2 lg:gap-x-16 lg:gap-y-6 xl:grid-cols-1 xl:grid-rows-1 xl:gap-x-8">
                     <h1 className="max-w-2xl text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl lg:col-span-2 xl:col-auto">
-                        Sauder's Premier Investment Group, focused on generating returns and educating members                    </h1>
+                        Sauders Premier Investment Group, focused on generating returns and educating members                    </h1>
                     <div className="mt-6 max-w-xl lg:mt-0 xl:col-end-1 xl:row-start-1">
                         <p className="text-lg leading-8 text-gray-600">
                             Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt
