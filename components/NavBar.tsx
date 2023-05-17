37c62dfaf402da8666cad790e1e7a10ef849a851
hypecarson <carson.lecky@gmail.com>
2023-05-16 17:40:43 -0700

    fix OG?

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -40,7 +40,9 @@ export default function NavBar() {
 
     return (
         <>
-
+            <Head>
+                <meta property="og:image" content="ogicon.png" />
+            </Head>
             <header className="bg-white">
                 <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
                     <div className="flex lg:flex-1">
