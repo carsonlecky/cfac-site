6272e94357f631f3c2a8bc8596e51ac85f9b5c51
hypecarson <carson.lecky@gmail.com>
2023-05-23 18:27:09 -0700

    change word

--- components/ExecTeam.tsx
+++ components/ExecTeam.tsx
@@ -20,7 +20,7 @@ export default function ExecTeam() {
         <div className="bg-white py-12">
             <div className="mx-auto max-w-7xl px-6 lg:px-8">
                 <div className="mx-auto max-w-2xl sm:text-center">
-                    <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Executive Team</h2>
+                    <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Management Team</h2>
                     <p className="mt-6 text-lg leading-8 text-gray-600">
                     </p>
                 </div>
