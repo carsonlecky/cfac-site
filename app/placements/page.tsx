9096a6cf7365ecb3ae1cf2d5ff67f88b7c082e25
hypecarson <carson.lecky@gmail.com>
2023-05-31 18:34:23 -0700

    k

--- app/placements/page.tsx
+++ app/placements/page.tsx
@@ -7,9 +7,9 @@ export default function Alumni() {
         <>
             <div className="bg-white px-6 py-12 lg:px-8">
                 <div className="mx-auto max-w-2xl text-center">
-                    <h2 className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Alumni Placements</h2>
+                    <h2 className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Placements</h2>
                     <p className="mt-6 text-lg leading-8 text-gray-600">
-                        CFAC Portfolio Management takes great pride in the achievements of our distinguished members, who have successfully secured placements in the following firms and many more.
+                        CFAC Portfolio Management takes great pride in the achievements of our distinguished members, who have successfully secured placements in the following firms and many more:
                     </p>
                 </div>
                 <LogoCloud />
