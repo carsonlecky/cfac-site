b474c08651093042010deacc4aca199591133a0a
hypecarson <carson.lecky@gmail.com>
2023-06-12 10:48:56 -0700

    rewerd

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -10,7 +10,7 @@ export default function Team() {
                 <div className="mx-auto max-w-2xl text-center">
                     <h2 className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Team</h2>
                     <p className="mt-6 text-lg leading-8 text-gray-600">
-                        CFAC Portfolio Management is split into two distinct teams: The Management team oversees the events, philanthropy, community representation and growth of the club. The Investment team manages the virtual portfolio’s investments.
+                        CFAC Portfolio Management operates with two distinct teams, each playing a vital role in our organization. The Management team is responsible for overseeing events, philanthropic initiatives, community representation, and the overall growth of the club. On the other hand, the Investment team takes charge of managing investments within our virtual portfolio. Together, these teams contribute to the success and effectiveness of CFAC Portfolio Management.
                     </p>
                 </div>
             </div>
